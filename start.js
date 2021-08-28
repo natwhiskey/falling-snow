ccaa090b51adcebb4938f1905eed979a8828adef
natwhiskey <kimngza@hotmail.com>
2021-08-28 22:48:27 +0700

    format code

--- start.js
+++ start.js
@@ -1,50 +1,50 @@
- (() => {
-    function setup(){
+(() => {
+    function setup() {
         const canvas = document.getElementById('falling-snow-canvas');
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
         return {
-        canvas,
-        canvasContext: canvas.getContext('2d'),
-        numberOfSnowBalls: 350
+            canvas,
+            canvasContext: canvas.getContext('2d'),
+            numberOfSnowBalls: 350
         }
     }
 
-    function random(max, min){
-       const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomNumber; 
+    function random(max, min) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        return randomNumber;
 
     }
 
-    function drawSnowBalls(canvasContext, snowBalls){
+    function drawSnowBalls(canvasContext, snowBalls) {
         canvasContext.beginPath();
-        canvasContext.arc(snowBalls.x, snowBalls.y,snowBalls.radius, snowBalls.opacity, Math.PI * 2 *snowBalls.radius);
+        canvasContext.arc(snowBalls.x, snowBalls.y, snowBalls.radius, snowBalls.opacity, Math.PI * 2 * snowBalls.radius);
         canvasContext.fillStyle = `rgba(255, 255, 255, ${snowBalls.opacity})`;
         canvasContext.fill();
     }
 
-    function createSnowBalls(canvas, numberOfSnowBalls){
-      const snowBallPropety = [...Array(numberOfSnowBalls)].map(()=>{
-          return {
-              x: random(canvas.width, 0),
-              y: random(canvas.height, 0),
-              radius: random(1, 5),
-              opacity: random(4, 2),
-              speedX: random (5, -5), 
-              speedY: random (3, 1),
-            } 
+    function createSnowBalls(canvas, numberOfSnowBalls) {
+        const snowBallPropety = [...Array(numberOfSnowBalls)].map(() => {
+            return {
+                x: random(canvas.width, 0),
+                y: random(canvas.height, 0),
+                radius: random(1, 5),
+                opacity: random(4, 2),
+                speedX: random(5, -5),
+                speedY: random(3, 1),
+            }
         });
-      return snowBallPropety;
-      
+        return snowBallPropety;
+
     }
 
-    function moveSnowBalls(canvas, snowBall){
+    function moveSnowBalls(canvas, snowBall) {
         snowBall.x += snowBall.speedX;
         snowBall.y += snowBall.speedY;
 
         if (snowBall.x > canvas.width) {
-            snowBall.x = 0;            
-        }   else if (snowBall.x < 0) {
+            snowBall.x = 0;
+        } else if (snowBall.x < 0) {
             snowBall.x = canvas.width;
         }
         if (snowBall.y > canvas.height) {
@@ -54,19 +54,19 @@
         }
     }
 
-    function run(){
-    const {canvas, canvasContext, numberOfSnowBalls} = setup();
-    const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
-    setInterval(() => {
-        canvasContext.clearRect(0,0, canvas.width, canvas.height);
-        snowBalls.forEach((snowBall) => {
-            drawSnowBalls(canvasContext, snowBall);
-        });
-        snowBalls.forEach((snowBall) => {
-            moveSnowBalls(canvas, snowBall);
-        });
-    },40);
+    function run() {
+        const { canvas, canvasContext, numberOfSnowBalls } = setup();
+        const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
+        setInterval(() => {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            snowBalls.forEach((snowBall) => {
+                drawSnowBalls(canvasContext, snowBall);
+            });
+            snowBalls.forEach((snowBall) => {
+                moveSnowBalls(canvas, snowBall);
+            });
+        }, 40);
     }
     run();
 
- } )(); 
+})();
