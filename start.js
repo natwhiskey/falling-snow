c1ddbeea4e4e9cda2ea0a7e3e70d4f2ba68d66d0
natwhiskey <kimngza@hotmail.com>
2021-08-28 19:14:37 +0700

    create function setup for use canvas window and set variable numberofsnowballs

--- start.js
+++ start.js
@@ -1,5 +1,16 @@
  (() => {
+    function setup(){
+        const canvas = document.getElementById('falling-snow-canvas');
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        return {
+        canvas,
+        canvasContext: canvas.getContext('2d'),
+        numberOfSnowBalls: 350
+        }
+    }
     function run(){
+    const {canvas, canvasContext, numberOfSnowBalls} = setup();
     }
     run();
 
